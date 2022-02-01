809f6157f7b2afb9c025569e5dc925bccec3606b
Vander Idme <bugdalf@gmail.com>
2022-02-01 16:02:26 -0500

    refactor y optmizacion

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 class CalculadorDeArea {
-  calcularAreaCuadrado(l) {
-    return l*l
+  calcularAreaCuadrilatero(l1=1, l2=1) {
+    if (l2 == 1) l2 = l1
+
+    return l1*l2
+  }
+  calcularAreaRectangulo(l, b) {
+    return l*b
   }
 }
 
--- main.spec.js
+++ main.spec.js
@@ -3,9 +3,14 @@ const CalculadorDeArea = require("./main")
 
 describe('Calculador de areas', () => {
   const calculadorDeArea = new CalculadorDeArea()
-  it('El area de un cuadrado de 5x5 debe dar 25', ()=>{
-    const res = calculadorDeArea.calcularAreaCuadrado(5)
+  it('El area de un cuadrilatero con 1 argumento', ()=>{
+    const res = calculadorDeArea.calcularAreaCuadrilatero(5)
     expect(res).toBe(25)
+  })
+
+  it('El area de un cuadrilatero con 2 argumento', ()=>{
+    const res = calculadorDeArea.calcularAreaCuadrilatero(10,2)
+    expect(res).toBe(20)
   })
 
 })
